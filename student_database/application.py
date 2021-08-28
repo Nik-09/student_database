589003f2cbbb2068137ba4e0229f584ad1336cc9
Nik-09 <nikhil.agarwal.2019@gmail.com>
2021-08-28 22:32:05 +0530

    Restructured some methods.

--- student_database/application.py
+++ student_database/application.py
@@ -23,8 +23,6 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
-# Custom filter
-app.jinja_env.filters["usd"] = usd
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
@@ -32,21 +30,53 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///student.db")
 
-# Make sure API key is set
-#if not os.environ.get("API_KEY"):
-#    raise RuntimeError("API_KEY not set")
 
-
+# Essential methods connected with corresponding routes.
 @app.route("/")
 @login_required
 def index():
-    """Show portfolio of stocks"""
+    """Show login page"""
     return render_template("index.html")
 
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method=="GET":
+        return render_template("register.html")
+    else:
+        username = request.form.get("username")
+        if (not username):
+            return apology("You must provide Username.")
+        password = request.form.get("password")
+        if (not password):
+            return apology("You must provide Password.")
+        confirmation = request.form.get("confirmation")
+        if (not confirmation):
+            return apology("You must confirm your password.")
+        if (password != confirmation):
+            return apology("Password must match.")
+        
+        id_type = request.form.get("type")
+        print(id_type)
+
+        # Check if the username already exists or not.
+        lis = db.execute("SELECT username FROM USERS")
+        user_exists = False
+        for j in range(len(lis)):
+            if username == lis[j]['username']:
+                user_exists = True
+                break
+
+        if user_exists:
+            return apology("User already exist")
+        else:
+            k = generate_password_hash(password)
+            db.execute("INSERT INTO USERS (username, hash, type) VALUES (?, ?, ?)", username, k, id_type)
+    return redirect("/")
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -54,7 +84,7 @@ def login():
     """Log user in"""
 
     # Forget any user_id
-    session.clear()
+    # session.clear()
 
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
@@ -68,7 +98,7 @@ def login():
             return apology("must provide password", 403)
 
         # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = :username",
+        rows = db.execute("SELECT * FROM USERS WHERE username = :username",
                           username=request.form.get("username"))
 
         # Ensure username exists and password is correct
@@ -99,8 +129,8 @@ def logout():
 
 @app.route("/search", methods=["GET", "POST"])
 @login_required
-def seach():
-    """Get stock quote."""
+def search():
+    """Get student record."""
     if request.method == "GET":
         return render_template("search.html")
     else:
@@ -111,42 +141,14 @@ def seach():
         else:
             name = request.form.get("name")
             std = request.form.get("class")
-            rows = db.execute("select * from stud where name=? and class=?",name, std)
-            print(rows)
-            return render_template("profile.html",rows=rows)
+            rows = db.execute("SELECT * FROM stud WHERE name=? and class=?",name, std)
+            remaining_month_fees = datetime.datetime.now().month 
+            
+            print(remaining_month_fees)
+            return render_template("profile.html",rows=rows, remaining_month_fees=remaining_month_fees)
 
     return apology("TODO")
 
-
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    if request.method=="GET":
-        return render_template("register.html")
-    else:
-        username = request.form.get("username")
-        if (not username):
-            return apology("You must provide Username.")
-        password = request.form.get("password")
-        if (not password):
-            return apology("You must provide Password.")
-        confirmation = request.form.get("confirmation")
-        if (not confirmation):
-            return apology("You must confirm your password.")
-        if (password != confirmation):
-            return apology("Password must match.")
-
-        lis = db.execute("select username from users")
-        flag = 0
-        for j in range(len(lis)):
-            if username == lis[j]['username']:
-                flag = 1
-                break
-        if (flag):
-            return apology("User already exist")
-        else:
-            k = generate_password_hash(password)
-            db.execute("insert into users (username, hash) values (?, ?)", username, k)
-    return redirect("/")
 
 
 @app.route("/enter", methods=["GET", "POST"])
@@ -159,7 +161,14 @@ def enter():
         std = request.form.get("class")
         fees = request.form.get("fees")
         month = request.form.get("month")
-        db.execute("insert into stud ('name', 'class', 'fees', 'month') values (?,?,?,?)", name, std, fees, month)
+        print(month)
+        
+        # Validating student data.
+        if not name:
+            return apology('Enter student name.')
+        if not month:
+            return apology('Enter month of joining.')
+        db.execute("INSERT INTO stud ('name', 'class', 'fees', 'month') VALUES (?,?,?,?)", name, std, fees, month)
         return render_template("unique.html")
 
 
